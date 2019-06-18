7d5a377617d6415ed9770b2141707f2c3d599a1f
caohaige <caohaige@dayukeji.com>
2019-06-18 15:45:58 +0800

    saga: random string

--- engine/utils/random.go
+++ engine/utils/random.go
@@ -94,3 +94,15 @@ func Shift(sarray []interface{}) error {
 	}
 	return nil
 }
+
+//GetRandomString
+func GetRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
